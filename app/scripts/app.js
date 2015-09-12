5708fb288b055d4d06883c3c18ce9d752023bf6a
Eric Soyke <ESoyke@pscufs.com>
2015-09-12 11:44:26 -0400

    Initial commit

---
+++ app/scripts/app.js
@@ -0,0 +1,35 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name ngPsinvoicesApp
+ * @description
+ * # ngPsinvoicesApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('ngPsinvoicesApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl',
+        controllerAs: 'main'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl',
+        controllerAs: 'about'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name ngPsinvoicesApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the ngPsinvoicesApp
+ */
+angular.module('ngPsinvoicesApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
